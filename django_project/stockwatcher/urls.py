cd2be2c80f4602507655df66296ce5946d950ca9
FowadIjaz <63612037+FowadIjaz@users.noreply.github.com>
2021-08-07 12:20:55 -0400

    displayed stock data on html

--- django_project/stockwatcher/urls.py
+++ django_project/stockwatcher/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
+from .views import StockListView
 
-urlpatterns = [  path('home/', views.home, name = "stockwatcher-home")]
+# urlpatterns = [  path('home/', views.home, name = "stockwatcher-home")]
+
+urlpatterns = [  path('home/', StockListView.as_view(), name = "stockwatcher-home")]
