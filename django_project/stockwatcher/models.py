2803287c2dab378d83809663e3c7bff2cecf21ab
Fowad Ijaz <63612037+FowadIjaz@users.noreply.github.com>
2021-08-07 11:58:15 -0400

    api scraping script implemented (#5)
    * api scraping script implemented
    
    * Delete u.txt
    
    * Delete disclosure-18-07-2021
    
    * Delete db.sqlite3

--- django_project/stockwatcher/models.py
+++ django_project/stockwatcher/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Stock(models.Model):
+    ticker = models.TextField(blank=True, null=True)
+    asset_description = models.TextField(blank=True, null=True)
+    asset_type = models.TextField(blank=True, null=True)
+    amount = models.TextField(blank=True, null=True)
+    type = models.TextField(blank=True, null=True)
+    transaction_date = models.DateTimeField(blank=True, null=True)
+    senator = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'StockTable'
--- django_project/stockwatcher/views.py
+++ django_project/stockwatcher/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Stock
 # Create your views here.
 
 
 def home(request):
     context = {}
     return render(request, 'stockwatcher/home.html', context)
+
+class StockListView(ListView):
+  model = Stock
+  template_name = 'stockwatcher/home.html' # <app>/<model>_<viewtype>.html
+  context_object_name = 'stocks'
+  ordering = ['-date_posted']
+  paginate_by = 5
