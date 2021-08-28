81e5b6cf14f301da27b5ae32eb10958a8b75272c
FowadIjaz <63612037+FowadIjaz@users.noreply.github.com>
2021-08-28 13:49:54 -0400

    got about graph and contact pages working

--- django_project/stockwatcher/urls.py
+++ django_project/stockwatcher/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
-from .views import StockListView
+from .views import StockListView, StockGraphView
 
 # urlpatterns = [  path('home/', views.home, name = "stockwatcher-home")]
-urlpatterns = [  path('', StockListView.as_view(), name = "stockwatcher-home")]
+urlpatterns = [  path('', StockListView.as_view(), name = "stockwatcher-home"),
+                path('graphs/', StockGraphView.as_view(),name="stockwatcher-graph"),
+                path('about/', views.about, name = "stockwatcher-about"),
+                path('contact/', views.contact, name = "stockwatcher-contact"),]
 
--- django_project/stockwatcher/views.py
+++ django_project/stockwatcher/views.py
@@ -1,5 +1,6 @@
+from typing import List
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Stock
 # Create your views here.
 
@@ -13,4 +14,15 @@ class StockListView(ListView):
   template_name = 'stockwatcher/home.html' # <app>/<model>_<viewtype>.html
   context_object_name = 'stocks'
   ordering = ['id']
-#   paginate_by = 100
+  paginate_by = 500
+
+class StockGraphView(ListView):
+  model = Stock
+  template_name = 'stockwatcher/graphs.html'
+  context_object_name = 'stocks'
+
+def about(request):
+  return render(request, 'stockwatcher/about.html', {'title': 'About'})
+
+def contact(request):
+  return render(request, 'stockwatcher/contact.html', {'title': 'Contact'})
