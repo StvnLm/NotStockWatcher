8a0aef9b414a2505d6b1c6ff2e5ce33a7bc37e28
steven <admin@example.com>
2021-09-06 12:39:22 -0400

    add new page for stocks

--- django_project/stockwatcher/urls.py
+++ django_project/stockwatcher/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 from .views import StockListView, StockGraphView
 
-# urlpatterns = [  path('home/', views.home, name = "stockwatcher-home")]
-urlpatterns = [  path('', StockListView.as_view(), name = "stockwatcher-home"),
+urlpatterns = [ path('', views.home, name = "stockwatcher-home"),
+                path('stocks/', StockListView.as_view(), name = "stockwatcher-stocks"),
                 path('graphs/', StockGraphView.as_view(),name="stockwatcher-graph"),
                 path('about/', views.about, name = "stockwatcher-about"),
                 path('contact/', views.contact, name = "stockwatcher-contact"),]
