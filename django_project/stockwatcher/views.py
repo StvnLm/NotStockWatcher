182419c4048e26990e5b6e88569ab4a0676a1563
FowadIjaz <63612037+FowadIjaz@users.noreply.github.com>
2021-09-06 12:19:55 -0400

    started chart.js in graphs.html

--- django_project/stockwatcher/views.py
+++ django_project/stockwatcher/views.py
@@ -2,6 +2,8 @@ from typing import List
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Stock
+import yfinance as yf
+from django.http import JsonResponse
 # Create your views here.
 
 
@@ -20,6 +22,16 @@ class StockGraphView(ListView):
   model = Stock
   template_name = 'stockwatcher/graphs.html'
   context_object_name = 'stocks'
+  paginate_by = 1
+
+  # def get_context_data(self, *args, **kwargs):
+  #   # Call the base implementation first to get a context 
+  #   tickerData = yf.Ticker("AAPL")
+  #   tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+  #   context = super().get_context_data(*args,**kwargs)
+  #   context['date'] = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
+  #   context['stockprice'] =  tickerDf.Close.values.tolist()
+  #   return JsonResponse(context)
 
 def about(request):
   return render(request, 'stockwatcher/about.html', {'title': 'About'})
